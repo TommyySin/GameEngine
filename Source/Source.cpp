19961266d009e17484b6e4ff35acfc3f05ecb9d7
TommyySin <yiktingsin@gmail.com>
2017-09-22 12:21:48 -0400

    Lab #2 submission

--- Source/Source.cpp
+++ Source/Source.cpp
@@ -11,19 +11,6 @@
 using namespace std;
 
 bool IsOnlyInstance() {
-	/*HANDLE handle = CreateMutex(NULL, TRUE, gameTitle);
-	if (GetLastError() != ERROR_SUCCESS) {
-		HWND hWnd = FindWindow(gameTitle, NULL);
-		if (hWnd) {
-			// An instance of your game is already running.
-			ShowWindow(hWnd, SW_SHOWNORMAL);
-			SetFocus(hWnd);
-			SetForegroundWindow(hWnd);
-			SetActiveWindow(hWnd);
-			return false;
-		}
-	}
-	return true;*/
 	CreateMutex(0, FALSE, "COOL GAME");
 	if (GetLastError() == ERROR_ALREADY_EXISTS) {
 		cout << "It has been running" << "\n";
