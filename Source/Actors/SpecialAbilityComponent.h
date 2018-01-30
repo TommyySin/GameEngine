5c37e21083421f09b2f24e5e95073b621d766c74
TommyySin <yiktingsin@gmail.com>
2018-01-30 12:14:01 -0500

    FirstLab

--- Source/Actors/SpecialAbilityComponent.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "AbilitiesInterface.h"
-
-class SpeicalAbility : public AbilitiesInterface
-{
-public:
-	virtual bool VInit(TiXmlElement* pData);
-	virtual void VApply(WeakActorPtr pActor);
-};
